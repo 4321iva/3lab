bf957cb6cf73c434e1d4a61f8ee225698ffb4cad
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-02 10:10:40 +0300

    commit

---
+++ lab3.4/lab3.4/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        long factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"Факторіал числа {number} = {factorial}");
+    }
+}
